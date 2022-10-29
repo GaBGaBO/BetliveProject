c64f51809fbd458265933b3d32124c88a894c6b1
GaBGaBO <101996684+GaBGaBO@users.noreply.github.com>
2022-10-29 12:47:32 -0700

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+// const color = document.querySelector(`.p`);
+// color.addEventListener(`mousemove`,function(){
+//    color.style.color = `white`;
+// })
+
+const chat = document.querySelector(`.makefreebet`).style.cursor = `pointer`;
